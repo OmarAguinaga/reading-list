7cf93bb222a5944787f24e9b9d65e9a7bc66642f
Omar Aguinaga <omar.aguinaga94@gmail.com>
2018-04-25 17:23:11 -0500

    Add graphql schemas for authors andd books

--- server/app.js
+++ server/app.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({}));
+mongoose.connect(
+  `mongodb://${process.env.DB_USER}:${
+    process.env.DB_PASS
+  }@ds157639.mlab.com:57639/reading-list`
+);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(3000, () => {
   console.log("now listening for reqests on port 3000");
